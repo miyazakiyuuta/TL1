1e78643ef8f3504fa91ac9b968d8c2b6cc9689bb
unburiburichi <k024g0091@g.neec.ac.jp>
2026-06-15 13:02:38 +0900

    TL1_01_05_再提出

--- level_editor.py
+++ level_editor.py
@@ -16,14 +16,6 @@ bl_info = {
     "category": "Object"
 }
 
-# メニュー項目描画
-def draw_menu_manual(self, context):
-    #self : 呼び出し元のクラスインスタンス。C++で言うthisポインタ
-    #context : カーソルを合わせた時のポップアップのカスタマイズなどに使用
-
-    #トップバーの「エディターメニュー」に項目(オペレータ)を追加
-    self.layout.operator("wm.url_open_preset", text="Manual", icon = 'HELP')
-
 # オペレータ 頂点を伸ばす
 class MYADDON_OT_stretch_vertex(bpy.types.Operator):
     bl_idname = "myaddon.myaddon_ot_stretch_vertex"
@@ -48,6 +40,7 @@ class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
         bpy.ops.mesh.primitive_ico_sphere_add()
         print("ICO球を生成しました。")
         return {'FINISHED'}
+    
     
 # オペレータ シーン出力
 class MYADDON_OT_export_scene(bpy.types.Operator):
@@ -88,7 +81,7 @@ class MYADDON_OT_export_scene(bpy.types.Operator):
 #トップバーの拡張メニュー
 class TOPBAR_MT_my_menu(bpy.types.Menu):
     #Blenderがクラスを識別するための固有の文字列
-    bl_idname = "TOPBAR_MT_my_menu"
+    bl_idname = "myaddon.topbar_mt_my_menu"
     #メニューのラベルとして表示される文字列
     bl_label = "MyMenu"
     #著者表示用の文字列
@@ -105,10 +98,9 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
                              text=MYADDON_OT_create_ico_sphere.bl_label)
         self.layout.operator(MYADDON_OT_export_scene.bl_idname,
                              text=MYADDON_OT_export_scene.bl_label)
-    # 既存のメニューにサブメニューを追加
-    def submenu(self, context):
-        # ID指定でサブメニューを追加
-        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+def draw_my_menu(self, context):
+    self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
 
 # Blenderに登録するクラスリスト
 classes = (
@@ -125,12 +117,12 @@ def register():
         bpy.utils.register_class(cls)
     
     #メニューに項目を追加
-    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
+    bpy.types.TOPBAR_MT_editor_menus.append(draw_my_menu)
     print("レベルエディタが有効化されました。")
 #Add-On無効化時コールバック
 def unregister():
     #メニューに項目を削除
-    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+    bpy.types.TOPBAR_MT_editor_menus.remove(draw_my_menu)
 
     # Blenderからクラスを削除
     for cls in classes:
